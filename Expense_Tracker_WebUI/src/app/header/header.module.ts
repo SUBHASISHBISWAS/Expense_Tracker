1c001cad5757490cd75e20f4b59a52458c55e5fe
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-03-29 23:09:21 +0530

    Update header.module.ts

--- Expense_Tracker_WebUI/src/app/header/header.module.ts
+++ Expense_Tracker_WebUI/src/app/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, AngularMaterialModule],
+  imports: [CommonModule, AngularMaterialModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
