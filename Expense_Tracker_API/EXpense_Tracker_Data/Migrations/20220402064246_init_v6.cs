3f20150a1e208972ed6351290b52e2205ee7eb94
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-04-02 12:14:32 +0530

    Migration is added

---
+++ Expense_Tracker_API/EXpense_Tracker_Data/Migrations/20220402064246_init_v6.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker_Data.Migrations
+{
+    public partial class init_v6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Expenses",
+                newName: "expenseDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "expenseCategory",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expenseCategory",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "expenseDate",
+                table: "Expenses",
+                newName: "CreatedDate");
+        }
+    }
+}
